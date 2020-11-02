24f8656f4a4fcc6482f9ccdf8a43e4dc7447f3fc
Prashant Mudgal <prashantmudgal000@gmail.com>
2020-11-02 11:09:44 +0530

    utilized row, column, expanded etc. widgets

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -9,21 +9,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      home: Home(),
+    );
+  }
+}
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Text("First flutter app"),
           centerTitle: true,
           backgroundColor: Colors.black12,
         ),
-        body: Center(
-          child: Text("Body of flutter app"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex:3,
+                  child: Image.asset('media/earth.jpg')
+                  ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex : 4,
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    color:Colors.green,
+                    child: Text("one"),
+                    ),
+                  ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    color:Colors.amber,
+                    child: Text("two"),
+                    ),
+                  ),
+              ],
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: (){},
           child: Text("click"),
         ),
-      ),
-    );
+      );
   }
 }
-
